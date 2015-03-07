d90f12b838259d128f5643520d46f946acf0990f
Robb Lewis <me@therobb.com>
2015-03-07 15:49:43 +0000

    Use real data from Jan 2015. New icons, generate checkboxes automatically

--- main.js
+++ main.js
@@ -3,15 +3,31 @@ $(document).ready(function() {
 	var map,
 		infoWindow = null,
 		markers = [],
-		boxes = $('#filters input:checkbox'),
+		boxes,
 		g_day = 31,
 		g_single = false;
-		g_crimes = [1,2,3,4,5];
+		g_crimes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+		g_index = [
+			"Anti-social behaviour",
+			"Bicycle theft",
+			"Criminal damage and arson",
+			"Other theft",
+			"Public order",
+			"Shoplifting",
+			"Violence and sexual offences",
+			"Burglary",
+			"Drugs",
+			"Vehicle crime",
+			"Robbery",
+			"Theft from the person",
+			"Other crime",
+			"Possession of weapons"
+		];
 
 	function initialize() {
 		var mapOptions = {
-			center: { lat: 50.794348, lng: -1.074721 },
-			zoom: 14,
+			center: { lat: 50.817293, lng: -1.073966 },
+			zoom: 13,
 			disableDefaultUI: true
 		};
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -20,18 +36,38 @@ $(document).ready(function() {
 
 	$.ajax({
 		dataType: "json",
-		url: 'newdata.json',
+		url: 'data.json',
 		success: function(data) {
 			for (var i = data.length - 1; i >= 0; i--) {
 			
-				var location = new google.maps.LatLng(data[i].lat,data[i].long);
+				var location = new google.maps.LatLng(data[i].Latitude,data[i].Longitude);
 
-				var content = '<p><strong>'+data[i].crime+'</strong></p><p>'+data[i].day+' December 2014<p>';
+				var content = '<p><strong>'+data[i].crime+'</strong></p><p>'+data[i].day+' January 2014<p>';
 
-				addPoint(location, content, data[i].crime_id, data[i].day);
+				var crime_id = getCrimeId(data[i].crime_type);
+				var crime_type = data[i].crime_type;
+
+				addPoint(location, content, crime_id, data[i].day);
+
+				if ( ! $('*[data-crime-id="'+crime_id+'"]').length) {
+					generateCheckbox(crime_id, crime_type)
+				}
 			};
+
+			addListeners();
 		}
 	});
+
+	function getCrimeId(type) {
+		return g_index.indexOf(type);
+	}
+
+	function generateCheckbox(id, type) {
+		var title = type;
+        var checkbox = document.createElement('p');
+        checkbox.innerHTML = '<label><input type="checkbox" data-crime-id="' + id + '" checked> <img src="icons/' + id + '.png"> ' + type + '</label>';
+        document.getElementById('filters').appendChild(checkbox);
+	}
 
 	function addPoint(position, content, crime_id, day) {
 		var marker = new google.maps.Marker({
@@ -58,28 +94,30 @@ $(document).ready(function() {
 		});
 	};
 
-	boxes.change(function(e){
-		var crime_id = $(e.currentTarget).data('crimeId');
-		var checked = $(e.currentTarget).prop('checked');
+	function addListeners() {
+		$('#filters input:checkbox').on('change', function(e) {
+			var crime_id = $(e.currentTarget).data('crimeId');
+			var checked = $(e.currentTarget).prop('checked');
 
-		setFilters();
+			setFilters();
 
-		$(markers).each(function(id, marker) {
-			if (marker.crime_id == crime_id) {
-				if (checked)
-					marker.setVisible(true);
-				else
-					marker.setVisible(false);
-			}
-		})
-	});
+			$(markers).each(function(id, marker) {
+				if (marker.crime_id == crime_id) {
+					if (checked)
+						marker.setVisible(true);
+					else
+						marker.setVisible(false);
+				}
+			})
+		});
+	};
 
 	$('input[type=range]').on("change mousemove", function() {
 		var day = $(this).val();
 
 		g_day = day;
 
-		$(this).next().html(day+getOrdinal(day));
+		$(this).next().html('January ' + day+getOrdinal(day) + ' 2015');
 
 		sortMarkers(day);
 	});
@@ -130,6 +168,7 @@ $(document).ready(function() {
 
 	function setFilters() {
 		g_crimes = [];
+		boxes = $('#filters input:checkbox');
 
 		$(boxes).each(function(box) {
 			if ($(this).prop('checked')) {
